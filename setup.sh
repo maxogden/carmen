1d9c6f6a5dd25cca2ed1e909cd2bad754c403280
Young Hahn <young@developmentseed.org>
2012-05-22 13:37:53 -0400

    Add setup script.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir -p tiles
+
+data=(
+  "carmen-city/layers/places.sqlite"
+  "carmen-country/layers/countries.sqlite"
+  "carmen-province/layers/provinces.sqlite"
+)
+
+for file in "${data[@]}"
+do
+  if [ ! -f $file ]; then
+    BASE=`basename $file`
+    DIR=`dirname $file`
+    mkdir -p $DIR
+    echo "Downloading $BASE..."
+    curl -s -o $file "https://s3.amazonaws.com/mapbox/carmen-data/$BASE"
+  fi
+done
+
