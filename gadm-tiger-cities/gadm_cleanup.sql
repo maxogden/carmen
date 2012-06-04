f41f8192bd04934de62a80d45848b14ec68bebab
AJ Ashton <aj@developmentseed.org>
2012-06-04 17:59:59 -0400

    Details for initial table prep

--- gadm-tiger-cities/gadm_cleanup.sql
+++ gadm-tiger-cities/gadm_cleanup.sql
@@ -1,3 +1,36 @@
+-- Create table with:
+-- shp2pgsql -W LATIN1 -s 4326 -c -D -I gadm2.shp gadm2 | psql gis
+
+-- Geometry needs to be clipped to the 900913 bounding box and reprojected to
+-- 900913. This is done in two steps via an extra column otherwise it fails.
+
+-- create & populate clipped column
+select  addgeometrycolumn(
+            'public',
+            'gadm2',
+            'geom_clipped',
+            4326,
+            'MULTIPOLYGON',
+            2
+        );
+
+update  gadm2
+set     geom_clipped = st_multi(st_intersection(geom,st_geomfromtext(
+            'MULTIPOLYGON(((-180 -85.084059268853,-180 85.084059268853,180 85.084059268853,180 -85.084059268853,-180 -85.084059268853)))')));
+
+-- create & populate reprojected column from clipped column
+select  addgeometrycolumn(
+            'public',
+            'gadm2',
+            'geom_merc',
+            900913,
+            'MULTIPOLYGON',
+            2
+        );
+
+update  gadm2
+set     geom_merc = st_transform(geom_clipped, 900913);
+
 -- create materialized columns to use for geocoding
 alter   table gadm2 add column carmen_name character varying(100);
 alter   table gadm2 add column carmen_search character varying(150);
