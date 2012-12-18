d4b6f3b57ab403e0e9dc9dfb6c4e964441d1726b
root <root@c.mapbox.com>
2012-12-18 13:43:49 -0500

    Better function calls and adjust Washington DC centerpoint.

--- ne-provinces/data.sh
+++ ne-provinces/data.sh
@@ -10,10 +10,11 @@ ogr2ogr -s_srs EPSG:900913 -t_srs EPSG:4326 -nlt MULTIPOLYGON -nln import -f "Po
 echo "
 CREATE TABLE data(id SERIAL PRIMARY KEY, name VARCHAR, search VARCHAR, lon FLOAT, lat FLOAT, bounds VARCHAR, area FLOAT);
 SELECT AddGeometryColumn('public', 'data', 'geometry', 4326, 'MULTIPOLYGON', 2);
-INSERT INTO data (id, geometry, name, search) SELECT ogc_fid, setsrid(wkb_geometry,4326), name_1 AS name, name_1||','||postal AS search FROM import;
-UPDATE data SET lon = x(pointonsurface(geometry)), lat = y(pointonsurface(geometry)), bounds = xmin(geometry)||','||ymin(geometry)||','||xmax(geometry)||','||ymax(geometry);
+INSERT INTO data (id, geometry, name, search) SELECT ogc_fid, st_setsrid(wkb_geometry,4326), name_1 AS name, name_1||','||postal AS search FROM import;
+UPDATE data SET lon = st_x(st_pointonsurface(geometry)), lat = st_y(st_pointonsurface(geometry)), bounds = st_xmin(geometry)||','||st_ymin(geometry)||','||st_xmax(geometry)||','||st_ymax(geometry);
 UPDATE data SET area = 0;
-UPDATE data SET area = st_area(st_geogfromwkb(geometry)) where within(geometry,geomfromtext('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))',4326));
+UPDATE data SET area = st_area(st_geogfromwkb(geometry)) where st_within(geometry,st_geomfromtext('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))',4326));
+UPDATE data SET lon = -77.0170942, lat = 38.9041485 WHERE id = '885'; -- District of Columbia
 " | psql -U postgres $TMP
 
 ogr2ogr -s_srs EPSG:4326 -t_srs EPSG:900913 -f "SQLite" -nln data ne-provinces.sqlite PG:"host=localhost user=postgres dbname=$TMP" data
