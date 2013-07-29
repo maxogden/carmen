f59ea78cef2641aa92c09dec1d9d723072c21b10
ian29 <ian.villeda@gmail.com>
2013-07-29 14:40:03 -0400

    proper attribution and instrucitons

--- wrapx.sql
+++ wrapx.sql
@@ -1,3 +1,35 @@
+-- Copyright (C) 2010 Sandro Santilli <strk@keybit.net>
+--
+-- This work is placed into the public domain.
+--
+-- SYNOPSYS:
+--   ST_WrapX(geometry, wrap float8, move float8)
+--
+-- DESCRIPTION:
+--
+--   This function split the input geometries and then
+--   moves every resulting component
+--   with bounding box falling on the right (for
+--   negative 'move') or on the left (for positive 'move')
+--   of given 'wrap' line in the direction specified
+--   by the 'move' parameter finally re-unioning the pieces
+--   togheter.
+--
+--   It is useful to "recenter" long-lat input to have features
+--   of interest not spawned from one side to the other.
+--
+-- USAGE:
+--
+--      -- Move all components of the given geometries whose bounding box
+--      -- falls completely on the left of x=0 to +360
+--      select ST_WrapX(the_geom, 0, 360);
+--
+--      -- Move all components of the given geometries whose bounding box
+--      -- falls completely on the left of x=-30 to +360
+--      select ST_WrapX(the_geom, -30, 360);
+--
+--
+
 CREATE OR REPLACE FUNCTION ST_WrapX(geom_in geometry, cutx float8, amount float8)
 RETURNS geometry AS $$
 DECLARE
