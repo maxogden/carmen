f70b7fcba85d08cd5ef71c8f63e1690af335bbc6
ian29 <ian.villeda@gmail.com>
2013-07-29 14:35:51 -0400

    split geometries that cross date line, throw out anything below -85 lat (sorry penguins)

--- qs-admin1/data.sh
+++ qs-admin1/data.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 set -e -u
 
+echo "setting up..."
 TMP=`mktemp -d tmpXXXX`
-echo "downloading..."
 createdb -U postgres -T template_postgis $TMP
+psql -U postgres -d $TMP -f wrapx.sql
+echo "downloading..."
 curl -sfo $TMP/qs_adm1.zip http://static.quattroshapes.com/qs_adm1.zip
 unzip -q $TMP/qs_adm1.zip -d $TMP
 echo "importing..."
 ogr2ogr \
 	-nlt MULTIPOLYGON \
 	-wrapdateline \
-	-clipdst -180 -85, 180 85 \
 	-nln import \
 	-f "PostgreSQL" PG:"host=localhost user=postgres dbname=$TMP" \
 	$TMP/qs_adm1.shp
 
 
 echo "
-CREATE TABLE data(id SERIAL PRIMARY KEY, name VARCHAR, search VARCHAR, lon FLOAT, lat FLOAT, bounds VARCHAR, area FLOAT);
-SELECT AddGeometryColumn('public', 'data', 'geometry', 4326, 'MULTIPOLYGON', 2);
+CREATE TABLE data(id SERIAL PRIMARY KEY, name VARCHAR, geometry GEOMETRY(Geometry, 4326), search VARCHAR, lon FLOAT, lat FLOAT, bounds VARCHAR, area FLOAT);
 INSERT INTO data (id, geometry, name, search)
-	SELECT ogc_fid, st_setsrid(wkb_geometry,4326), qs_a1 AS name, coalesce(qs_a1||','||qs_a1_alt, qs_a1) AS search FROM import;
+	SELECT ogc_fid, st_setsrid(st_wrapx(wkb_geometry, 180, -180),4326), qs_a1 AS name, coalesce(qs_a1||','||qs_a1_alt, qs_a1) AS search FROM import;
 UPDATE data SET
-	lon = st_x(st_pointonsurface(geometry)),
-	lat = st_y(st_pointonsurface(geometry)),
-	bounds = st_xmin(geometry)||','||st_ymin(geometry)||','||st_xmax(geometry)||','||st_ymax(geometry);
+    lon = st_x(st_pointonsurface(geometry)),
+    lat = st_y(st_pointonsurface(geometry)),
+    bounds = st_xmin(geometry)||','||st_ymin(geometry)||','||st_xmax(geometry)||','||st_ymax(geometry);
 UPDATE data SET area = 0;
 UPDATE data SET area = st_area(st_geogfromwkb(geometry)) where st_within(geometry,st_geomfromtext('POLYGON((-180 -90, -180 90, 180 90, 180 -90, -180 -90))',4326));
 " | psql -U postgres $TMP
