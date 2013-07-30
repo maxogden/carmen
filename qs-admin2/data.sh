f447474cac8ba6d60697e184cc8b6a47868e086b
ian29 <ian.villeda@gmail.com>
2013-07-29 22:14:31 -0400

    qs admin 2 script

--- qs-admin2/data.sh
+++ qs-admin2/data.sh
@@ -6,20 +6,21 @@ TMP=`mktemp -d tmpXXXX`
 createdb -U postgres -T template_postgis $TMP
 psql -U postgres -d $TMP -f wrapx.sql
 echo "downloading..."
-curl -sfo $TMP/qs_adm1.zip http://static.quattroshapes.com/qs_adm1.zip
-unzip -q $TMP/qs_adm1.zip -d $TMP
+curl -sfo $TMP/qs_adm2.zip http://static.quattroshapes.com/qs_adm2.zip
+unzip -q $TMP/qs_adm2.zip -d $TMP
 echo "importing..."
+export PGCLIENTENCODING=latin1
 ogr2ogr \
 	-nlt MULTIPOLYGON \
 	-nln import \
 	-f "PostgreSQL" PG:"host=localhost user=postgres dbname=$TMP" \
-	$TMP/qs_adm1.shp
+	$TMP/qs_adm2.shp
 
 echo "cleaning..."
 echo "
 CREATE TABLE data(id SERIAL PRIMARY KEY, name VARCHAR, geometry GEOMETRY(Geometry, 4326), search VARCHAR, lon FLOAT, lat FLOAT, bounds VARCHAR, area FLOAT);
 INSERT INTO data (id, geometry, name, search)
-	SELECT ogc_fid, st_setsrid(st_wrapx(wkb_geometry, 180, -180),4326), qs_a1 AS name, coalesce(qs_a1||','||qs_a1_alt, qs_a1) AS search FROM import;
+	SELECT ogc_fid, st_setsrid(st_wrapx(wkb_geometry, 180, -180),4326), qs_a1 AS name, coalesce(qs_a2||','||qs_a2_alt, qs_a2) AS search FROM import;
 UPDATE data SET
     lon = st_x(st_pointonsurface(geometry)),
     lat = st_y(st_pointonsurface(geometry)),
@@ -35,10 +36,10 @@ ogr2ogr \
 	-wrapdateline \
 	-f "SQLite" \
 	-nln data \
-	qs-adm1.sqlite \
+	qs-adm2.sqlite \
 	PG:"host=localhost user=postgres dbname=$TMP" data
 echo "cleaning up..."
 dropdb -U postgres $TMP
 rm -rf $TMP
 
-echo "Written to qs-adm1.sqlite."
+echo "Written to qs-adm2.sqlite."
