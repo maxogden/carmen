6dd9df9d5706ec7c1de98ae9cd0dbfce55cbd79a
Dane Springmeyer <dane@dbsgeo.com>
2013-08-29 23:47:35 -0700

    test protobuf in benchmark

--- bench/load-query.js
+++ bench/load-query.js
@@ -17,12 +17,12 @@ var ids = [
     141956873251072, // 0
 ];
 
-var name = 'total time c++ x';
+var name = 'total time capnproto x';
 console.time(name + times);
 for (var i=0;i<times;++i) {
     console.time('  total');
     var cache = new CXXCache('a', 2);
-    cache.load(getter(type,shard,'.packed'), type, shard);
+    cache.load(getter(type,shard,'.packed'), type, shard, 'capnproto');
     console.time('  3 gets');
     assert.deepEqual(cache.get('grid',52712469173248),[[104101,1100010900000591]]);
     assert.deepEqual(cache.get('grid',98071753006080),[[10996,1100005350000776,1100005350000775]]);
@@ -36,6 +36,27 @@ for (var i=0;i<times;++i) {
     if (i==0) console.timeEnd('  total');
 }
 console.timeEnd(name + times);
+
+var name = 'total time protobuf x';
+console.time(name + times);
+for (var i=0;i<times;++i) {
+    console.time('  total');
+    var cache = new CXXCache('a', 2);
+    cache.load(getter(type,shard,'.pbf'), type, shard, 'protobuf');
+    console.time('  3 gets');
+    assert.deepEqual(cache.get('grid',52712469173248),[[104101,1100010900000591]]);
+    assert.deepEqual(cache.get('grid',98071753006080),[[10996,1100005350000776,1100005350000775]]);
+    assert.deepEqual(cache.get('grid',141956873251072),[[109619,1100010400000685]]);
+    if (i==0) console.timeEnd('  3 gets');
+    console.time('  30 gets');
+    for (var j=0;j<30;++j) {
+        assert.deepEqual(cache.get('grid',0),undefined);
+    }
+    if (i==0) console.timeEnd('  30 gets');
+    if (i==0) console.timeEnd('  total');
+}
+console.timeEnd(name + times);
+
 
 /*
 
--- convert.js
+++ convert.js
@@ -3,9 +3,6 @@ var JSCache = require('./cache');
 var fs = require('fs');
 var assert = require('assert');
 
-var from = process.argv[2];
-var to = process.argv[3];
-
 function getter(type, shard, file_ext) {
     return fs.readFileSync(__dirname + '/test/fixtures/' + type + '.' + shard + file_ext);
 };
@@ -26,3 +23,23 @@ for (var i=0;i<=2;++i) {
 	});
 }
 
+
+function getter2(type, shard, file_ext) {
+    return fs.readFileSync(__dirname + '/test/fixtures/big/' + type + '.' + shard + file_ext);
+};
+
+for (var i=0;i<1;++i) {
+    var shard = i;
+    ['grid','term'].forEach(function(type){
+        var jscache = new JSCache('a', shard);
+        var cxxcache = new CXXCache('a', shard);
+        jscache.load(getter2(type, shard,'.json'), type, shard);
+        cxxcache.loadJSON(jscache[type][shard],type,shard);
+        var cxxsorted = cxxcache.list(type,shard).sort();
+        var jssorted = jscache.list(type,shard).sort();
+        assert.deepEqual(jssorted,cxxsorted);
+        var filename = __dirname + '/test/fixtures/big/' + type + '.' + shard;
+        fs.writeFileSync(filename + '.pbf',cxxcache.pack(type,shard,'protobuf'));
+        fs.writeFileSync(filename + '.packed',cxxcache.pack(type,shard,'capnproto'));
+    });
+}
