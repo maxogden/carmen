dd28e7d1ae5421b7904f36157659e9418dec8f03
Dane Springmeyer <dane@dbsgeo.com>
2013-09-05 16:54:59 -0700

    ensure shardlevel is a number not string

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function Carmen(options) {
         if (source.open) {
             source.getInfo(function(err, info) {
                 if (err) return done(err);
-                source._carmen = source._carmen || new Cache(key, info.shardlevel || 0);
+                source._carmen = source._carmen || new Cache(key, +(info.shardlevel || 0));
                 source._carmen.zoom = info.maxzoom;
                 return done();
             });
@@ -85,7 +85,7 @@ function Carmen(options) {
                 if (err) return done(err);
                 source.getInfo(function(err, info) {
                     if (err) return done(err);
-                    source._carmen = source._carmen || new Cache(key, info.shardlevel || 0);
+                    source._carmen = source._carmen || new Cache(key, +(info.shardlevel || 0)));
                     source._carmen.zoom = info.maxzoom;
                     return done();
                 });
