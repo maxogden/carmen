de947f058c51fd83ad0f93b6c750d75624042a17
ian29 <ian.villeda@gmail.com>
2013-11-07 19:42:30 -0500

    join tzid to offset values, actually import tzids.csv

--- tz/data.sh
+++ tz/data.sh
@@ -13,20 +13,21 @@ shp2pgsql -s 4326 $TMP/world/tz_world_mp.shp import | psql -d $TMP
 
 # download + import offsets
 curl -sfo $TMP/tzids.csv http://www.snae.net/tzids.csv
-psql -d $TMP -c "CREATE TABLE offsets (tzid VARCHAR, utc int, windows_id VARCHAR, windows_display VARCHAR)"
-sed '1d' $TMP | psql -d $TMP -c "COPY offsets FROM STDIN"
+psql -d $TMP -c "CREATE TABLE offsets (tzid VARCHAR, utc FLOAT, windows_id VARCHAR, windows_display VARCHAR)"
+sed '1d' $TMP/tzids.csv | psql -d $TMP -c "COPY offsets FROM STDIN WITH DELIMITER ',' CSV"
 
 echo "
 CREATE TABLE data(id SERIAL PRIMARY KEY, name VARCHAR, search VARCHAR, utc VARCHAR, lon FLOAT, lat FLOAT, bounds VARCHAR);
 SELECT AddGeometryColumn('public', 'data', 'geometry', 4326, 'MULTIPOLYGON', 2);
-INSERT INTO data (id, geometry, name, search, utc) SELECT ogc_fid, setsrid(wkb_geometry,4326), utc FROM import;
-UPDATE data SET lon = x(pointonsurface(geometry)), lat = y(pointonsurface(geometry)), bounds = xmin(geometry)||','||ymin(geometry)||','||xmax(geometry)||','||ymax(geometry);
-" | psql -U postgres $TMP
+INSERT INTO data (id, geometry, name, search, utc) SELECT a.gid, a.geom, a.tzid, a.tzid, b.utc FROM import a JOIN offsets b ON lower(a.tzid) = lower(b.tzid);
+UPDATE data SET lon = st_x(st_pointonsurface(geometry)), lat = st_y(st_pointonsurface(geometry)), bounds = st_xmin(geometry)||','||st_ymin(geometry)||','||st_xmax(geometry)||','||st_ymax(geometry);
+" | psql $TMP
 
+# -- UPDATE data SET lon = x(pointonsurface(geometry)), lat = y(pointonsurface(geometry)), bounds = xmin(geometry)||','||ymin(geometry)||','||xmax(geometry)||','||ymax(geometry);
 # todo: join tzid geom to offset
 
-ogr2ogr -s_srs EPSG:4326 -t_srs EPSG:900913 -f "SQLite" -nln data tz.sqlite PG:"host=localhost user=postgres dbname=$TMP" data
-dropdb -U postgres $TMP
+ogr2ogr -s_srs EPSG:4326 -t_srs EPSG:900913 -f "SQLite" -nln data tz.sqlite PG:"host=localhost dbname=$TMP" data
+dropdb $TMP
 rm -rf $TMP
 
 echo "Written to tz.sqlite."
