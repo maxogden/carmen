53ca692b0ab7191223df77d79ea51f2ae3e33ad6
ian29 <ian.villeda@gmail.com>
2013-11-11 19:59:53 -0500

    better format, brief description in comments

--- split.sh
+++ split.sh
@@ -1,8 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 set -e -u
 
-for country in $(sqlite3 $1 "select name from data order by name limit 10" | sed 's/\ /-/g')
+# takes a sqlite full of carmen-redy data (assumes unique `name` field)
+# and outputs one topojson for every record
+
+for country in $(sqlite3 $1 "select name from data order by name" | sed 's/\ /-/g')
 do
+    j=$(echo $country | sed 's/-/ /g')
+    ogr2ogr \
+        -f GeoJSON \
+        -s_srs EPSG:3857 \
+        -t_srs EPSG:4326 \
+        -sql "select * from data where name = '$j'" \
+        json/${country}.geojson $1
+
+    topojson \
+        --no-quantization \
+        -p name,search,lat,lon,bounds,area \
+        json/${country}.geojson \
+        -o json/${country}.json
+
+    rm -rf json/${country}.geojson
+done
+
 	# let i=0
 	# if [ -e json/${country}.geojson ]; then
 	# 	i=$(i + 1)
@@ -10,8 +30,3 @@ do
 	# 	country=${country}_${i}
 	# 	echo $country
 	# fi
-	j=$(echo $country | sed 's/-/ /g')
-	ogr2ogr -f GeoJSON -t_srs EPSG:4326 -sql "select * from data where name = '$j'" json/${country}.geojson $1
-	topojson --no-quantization json/${country}.geojson -o json/${country}.json
-	rm -rf json/${country}.geojson
-done
